e18d55accd2d8f33ff883a5f730392b7b1e16549
ignaciolopezc <ignaciolopezc@gmail.com>
2025-09-08 23:23:10 -0300

    rm package astro/vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
+// import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +13,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  adapter: vercel(),
+  // adapter: vercel(),
 });
