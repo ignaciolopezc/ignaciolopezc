36434c81e640db2548e270051479e848ff28fb36
ignaciolopezc <ignaciolopezc@gmail.com>
2025-09-13 02:25:18 -0300

    se quitaron las redirecciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-
-	// redirects: { '*': '/' },
-
+	adapter: vercel(),
+	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
-
-	adapter: vercel(),
-	integrations: [react()],
 });
